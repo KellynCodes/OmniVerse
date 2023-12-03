e5c256801bdc7cfd69957debe574a28f047f3473
BeckyyCodes <adegboye.tosin.r@gmail.com>
2023-12-03 05:52:19 +0100

    completed the navbar

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,13 +3,20 @@ export default function Home() {
     <main>
       <img src="./images/Frame.jpg" alt="" />
       <nav>
-        <div>
+        <div className="options">
           <ul>
-            <li>Shop</li>
+            <li className="shop">Shop <img src="./images/Vector-3.jpg" alt="" /> </li>
             <li>On Sale</li>
             <li>New Arrivals</li>
             <li>Brands</li>
           </ul>
+        </div>
+        <div className="search-container">
+          <img src="./images/Vector-1.png" alt="" />
+          <input type="text" name="search" placeholder="Search for products..." />
+        </div>
+        <div>
+          <img src="./images/Vector-2.jpg" alt="" />
         </div>
       </nav>
     </main>
