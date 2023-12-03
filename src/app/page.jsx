b64797283c9c74ce0fd2fa675ec5bbe45fb15335
BeckyyCodes <adegboye.tosin.r@gmail.com>
2023-12-03 21:50:06 +0100

    just finished the new arrivals and top selling product section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,11 +2,13 @@ export default function Home() {
   return (
     <main>
       <img src="./images/Frame.jpg" alt="" />
-       {/* ===== NAVBAR START ====== */}
+      {/* ===== NAVBAR START ====== */}
       <nav>
         <div className="options">
           <ul>
-            <li className="shop">Shop <img src="./images/Vector-3.jpg" alt="" /> </li>
+            <li className="shop">
+              Shop <img src="./images/Vector-3.jpg" alt="" />{" "}
+            </li>
             <li>On Sale</li>
             <li>New Arrivals</li>
             <li>Brands</li>
@@ -14,21 +16,29 @@ export default function Home() {
         </div>
         <div className="search-container">
           <img src="./images/Vector-1.png" alt="" />
-          <input type="text" name="search" placeholder="Search for products..." />
+          <input
+            type="text"
+            name="search"
+            placeholder="Search for products..."
+          />
         </div>
         <div>
           <img src="./images/Vector-2.jpg" alt="" />
         </div>
       </nav>
-       {/* ===== NAVBAR END ====== */}
+      {/* ===== NAVBAR END ====== */}
 
-       {/* HERO SECTION START */}
+      {/* HERO SECTION START */}
        <header className="hero">
         <img src="./images/hero.jpg" alt="" />
         <div className="hero-content">
           <div className="hero-content-width">
             <h1>FIND CLOTHES THAT MATCHES YOUR STYLE</h1>
-            <p>Browse through our diverse range of meticulously crafted garments, designed to bring out your individuality and cater to your sense of style.</p>
+            <p>
+              Browse through our diverse range of meticulously crafted garments,
+              designed to bring out your individuality and cater to your sense
+              of style.
+            </p>
             <button type="button">Shop Now</button>
             <div className="articles">
               <article>
@@ -46,16 +56,160 @@ export default function Home() {
             </div>
           </div>
         </div>
-       </header>
-       {/* HERO SECTION END */}
-       
-       <section className="arrivals">
-        <div>
-          <div>
+      </header>
+      {/* HERO SECTION END */}
 
+      <section className="arrivals">
+        <h1>NEW ARRIVALS</h1>
+        <div className="container">
+          <div className="card">
+            <img className="image" src="./images/image-7.jpg" alt="" />
+            <div className="article-content">
+              <p>T-SHIRT WITH TAPE DETAILS</p>
+              <article>
+                <div>
+                  <img src="./images/Star-1.png" alt="" />
+                  <img src="./images/Star-2.png" alt="" />
+                  <img src="./images/Star-3.png" alt="" />
+                  <img src="./images/Star-4.png" alt="" />
+                  <img src="./images/Star-6.png" alt="" />
+                </div>
+                <span>4.5/5</span>
+              </article>
+              <p className="second">$120</p>
+            </div>
+          </div>
+          <div className="card">
+            <img className="image" src="./images/image-8.png" alt="" />
+            <div className="article-content">
+              <p>T-SHIRT WITH TAPE DETAILS</p>
+              <article>
+                <div>
+                  <img src="./images/Star-1.png" alt="" />
+                  <img src="./images/Star-2.png" alt="" />
+                  <img src="./images/Star-3.png" alt="" />
+                  <img src="./images/Star-4.png" alt="" />
+                  <img src="./images/Star-6.png" alt="" />
+                </div>
+                <span>4.5/5</span>
+              </article>
+              <p className="second">$120</p>
+            </div>
+          </div>
+          <div className="card">
+            <img className="image" src="./images/image-9.png" alt="" />
+            <div className="article-content">
+              <p>T-SHIRT WITH TAPE DETAILS</p>
+              <article>
+                <div>
+                  <img src="./images/Star-1.png" alt="" />
+                  <img src="./images/Star-2.png" alt="" />
+                  <img src="./images/Star-3.png" alt="" />
+                  <img src="./images/Star-4.png" alt="" />
+                  <img src="./images/Star-6.png" alt="" />
+                </div>
+                <span>4.5/5</span>
+              </article>
+              <p className="second">$120</p>
+            </div>
+          </div>
+          <div className="card">
+            <img className="image" src="./images/image-10.png" alt="" />
+            <div className="article-content">
+              <p className="first">T-SHIRT WITH TAPE DETAILS</p>
+              <article>
+                <div>
+                  <img src="./images/Star-1.png" alt="" />
+                  <img src="./images/Star-2.png" alt="" />
+                  <img src="./images/Star-3.png" alt="" />
+                  <img src="./images/Star-4.png" alt="" />
+                  <img src="./images/Star-6.png" alt="" />
+                </div>
+                <span>4.5/5</span>
+              </article>
+              <p className="second">$120</p>
+            </div>
           </div>
         </div>
-       </section>
+        <button type="button"> View All </button>
+      </section>
+
+      <div className="border"></div>
+
+      <section className=" top-selling">
+        <h1>top selling</h1>
+        <div className="container">
+          <div className="card">
+            <img className="image" src="./images/image-7.jpg" alt="" />
+            <div className="article-content">
+              <p>T-SHIRT WITH TAPE DETAILS</p>
+              <article>
+                <div>
+                  <img src="./images/Star-1.png" alt="" />
+                  <img src="./images/Star-2.png" alt="" />
+                  <img src="./images/Star-3.png" alt="" />
+                  <img src="./images/Star-4.png" alt="" />
+                  <img src="./images/Star-6.png" alt="" />
+                </div>
+                <span>4.5/5</span>
+              </article>
+              <p className="second">$120</p>
+            </div>
+          </div>
+          <div className="card">
+            <img className="image" src="./images/image-8.png" alt="" />
+            <div className="article-content">
+              <p>T-SHIRT WITH TAPE DETAILS</p>
+              <article>
+                <div>
+                  <img src="./images/Star-1.png" alt="" />
+                  <img src="./images/Star-2.png" alt="" />
+                  <img src="./images/Star-3.png" alt="" />
+                  <img src="./images/Star-4.png" alt="" />
+                  <img src="./images/Star-6.png" alt="" />
+                </div>
+                <span>4.5/5</span>
+              </article>
+              <p className="second">$120</p>
+            </div>
+          </div>
+          <div className="card">
+            <img className="image" src="./images/image-9.png" alt="" />
+            <div className="article-content">
+              <p>T-SHIRT WITH TAPE DETAILS</p>
+              <article>
+                <div>
+                  <img src="./images/Star-1.png" alt="" />
+                  <img src="./images/Star-2.png" alt="" />
+                  <img src="./images/Star-3.png" alt="" />
+                  <img src="./images/Star-4.png" alt="" />
+                  <img src="./images/Star-6.png" alt="" />
+                </div>
+                <span>4.5/5</span>
+              </article>
+              <p className="second">$120</p>
+            </div>
+          </div>
+          <div className="card">
+            <img className="image" src="./images/image-10.png" alt="" />
+            <div className="article-content">
+              <p className="first">T-SHIRT WITH TAPE DETAILS</p>
+              <article>
+                <div>
+                  <img src="./images/Star-1.png" alt="" />
+                  <img src="./images/Star-2.png" alt="" />
+                  <img src="./images/Star-3.png" alt="" />
+                  <img src="./images/Star-4.png" alt="" />
+                  <img src="./images/Star-6.png" alt="" />
+                </div>
+                <span>4.5/5</span>
+              </article>
+              <p className="second">$120</p>
+            </div>
+          </div>
+        </div>
+        <button type="button"> View All </button>
+      </section>
     </main>
   );
 }
