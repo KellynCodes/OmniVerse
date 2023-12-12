f9b0940a576ef6a3a9ed3c3c223e392c7b0d1a19
kellyncodes <kellyncodes@gmail.com>
2023-12-12 23:09:10 +0100

    fix: build error on production

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { Web5 } from '@web5/api';
 import { Button } from "@/components/shared/Button";
 import { useSearchParams, useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
@@ -8,7 +7,7 @@ import Link from "next/link";
 import { CopyText } from "@/libs/services/clipboard";
 import Alert from "@/components/shared/alert/Alert";
 import { AlertDto } from "@/libs/types/Dtos/alert.dto";
-import ProtocolDefinition from './ProtocolDefinition';
+import ProtocolDefinition from "./ProtocolDefinition";
 
 const ConfigureProtocol = () => {
   const searchParams = useSearchParams();
@@ -18,11 +17,13 @@ const ConfigureProtocol = () => {
     isSuccessful: false,
     message: null,
   });
-  const [message, setMessage] = useState<{ isSuccess: boolean; text: string | null }>({
+  const [message, setMessage] = useState<{
+    isSuccess: boolean;
+    text: string | null;
+  }>({
     isSuccess: false,
     text: null,
   });
-
 
   const [web5, setWeb5] = useState<any>(null);
   const [myDid, setMyDid] = useState<any>(null);
@@ -33,6 +34,7 @@ const ConfigureProtocol = () => {
 
   useEffect(() => {
     const initWeb5 = async () => {
+      const { Web5 } = await import("@web5/api/browser");
       try {
         const { web5, did } = await Web5.connect();
         console.log(web5);
@@ -45,12 +47,10 @@ const ConfigureProtocol = () => {
     };
 
     initWeb5();
-
   }, []);
 
-
   const handleCopy = async () => {
-    if(myDid){
+    if (myDid) {
       let response = await CopyText(myDid);
       setCopyMessage(response);
       setTimeout(() => {
@@ -59,9 +59,8 @@ const ConfigureProtocol = () => {
     }
   };
 
-
   const handleSeeDid = () => {
-    setIsDidVisible(true)
+    setIsDidVisible(true);
   };
 
   const handleConfigProtocol = async () => {
@@ -71,12 +70,11 @@ const ConfigureProtocol = () => {
           definition: ProtocolDefinition,
         },
       });
-      setProtocolStatus((prevStatus:any) => {
+      setProtocolStatus((prevStatus: any) => {
         //console.log("Protocol Status before set: ", prevStatus);
         return status;
       });
       await protocol.send(myDid);
-
     } catch (error) {
       console.error("Error configuring protocol:", error);
     } finally {
@@ -96,9 +94,12 @@ const ConfigureProtocol = () => {
       console.log(protocols);
 
       if (protocols[0]) {
-        setQueryProtocolStatus('Installed protocol: ' + JSON.stringify(protocols[0].definition.protocol));
+        setQueryProtocolStatus(
+          "Installed protocol: " +
+            JSON.stringify(protocols[0].definition.protocol)
+        );
       } else {
-        setQueryProtocolStatus('No protocol found.');
+        setQueryProtocolStatus("No protocol found.");
       }
     } catch (error) {
       console.error("Error querying protocol:", error);
@@ -107,106 +108,95 @@ const ConfigureProtocol = () => {
     }
   };
 
-
   return (
-      <>
-        {copyMessage.isSuccessful ? (
-            <Alert successMessage={copyMessage.message} />
-        ) : (
-            <Alert errorMessage={copyMessage.message} />
-        )}
-        <div
-            className="w-full fixed min-h-[100vh] z-[999] bg-neutral-800 items-center justify-center inset-0"
-            onClick={() => router.push("/#new-arrivals")}
-        ></div>
-        <div className="z-[99999] w-[100vw] h-screen md:h-[80vh] md:w-[70vw] lg:w-[50vw] p-12 my-auto mx-auto flex flex-col items-center gap-5 justify bg-white rounded-md relative">
-          <h2 className="text-black text-lg font-normal">
-            Take 1 minute to configure your identity, skip if you have done that
-            already.
-          </h2>
-          <Link href="/#new-arrivals">
-            <Image
-                className="absolute right-[3%] top-8"
-                src="/svg/cut.svg"
-                alt="Close Menu"
-                width={20}
-                height={20}
-            />
-          </Link>
-          <div className="flex flex-col w-full">
-            <div
-                onClick={handleConfigProtocol}
-                className="w-full py-4 mb-4 rounded inline-flex items-center bg-gray-500 bg-opacity-10 opacity-70 justify-center text-center cursor-pointer"
-            >
-              <h3 className="text-black text-sm font-medium">Protocol Config</h3>
-
-            </div>
-            {protocolStatus && (
-                <p className="text-black text-sm font-medium mb-4 ml-4">
-                  {JSON.stringify(protocolStatus)}
-                </p>
-            )}
-
-
-
-            <div
-                onClick={handleQueryProtocol}
-                className="w-full py-4 mb-4 inline-flex items-center bg-gray-500  bg-opacity-10 opacity-70 justify-center text-center  cursor-pointer"
-            >
-              <h3 className="text-black text-sm font-medium">Protocol Query</h3>
-
-            </div>
-            {queryProtocolStatus && (
-                <p className="text-black text-sm font-medium mb-4 ml-4">
-                  {queryProtocolStatus}
-                </p>
-            )}
-
-            <div
-                onClick={handleSeeDid}
-                className="w-full py-4 inline-flex items-center bg-gray-500  bg-opacity-10 gap-2.5 opacity-70 justify-center text-center  cursor-pointer"
-            >
-              <h3 id="seeDidInfo" className="text-black text-sm font-medium">
-                See DID
-              </h3>
-            </div>
-            {isDidVisible && (
-                <div className="mt-4 p-4 bg-gray-200 rounded">
-                  <p className="text-black text-sm font-medium">Your DID:</p>
-                  <div className="overflow-auto max-h-40">
-                    <pre className="text-xs break-words">{myDid}</pre>
-                  </div>
-                </div>
-            )}
-
-
-
-
-          </div>
-          <Button
-              label="Proceed"
-              link={redirectUrl!}
-              className="w-full bg-accent py-4 rounded-md text-white text-center"
+    <>
+      {copyMessage.isSuccessful ? (
+        <Alert successMessage={copyMessage.message} />
+      ) : (
+        <Alert errorMessage={copyMessage.message} />
+      )}
+      <div
+        className="w-full fixed min-h-[100vh] z-[999] bg-neutral-800 items-center justify-center inset-0"
+        onClick={() => router.push("/#new-arrivals")}
+      ></div>
+      <div className="z-[99999] w-[100vw] h-screen md:h-[80vh] md:w-[70vw] lg:w-[50vw] p-12 my-auto mx-auto flex flex-col items-center gap-5 justify bg-white rounded-md relative">
+        <h2 className="text-black text-lg font-normal">
+          Take 1 minute to configure your identity, skip if you have done that
+          already.
+        </h2>
+        <Link href="/#new-arrivals">
+          <Image
+            className="absolute right-[3%] top-8"
+            src="/svg/cut.svg"
+            alt="Close Menu"
+            width={20}
+            height={20}
           />
+        </Link>
+        <div className="flex flex-col w-full">
+          <div
+            onClick={handleConfigProtocol}
+            className="w-full py-4 mb-4 rounded inline-flex items-center bg-gray-500 bg-opacity-10 opacity-70 justify-center text-center cursor-pointer"
+          >
+            <h3 className="text-black text-sm font-medium">Protocol Config</h3>
+          </div>
+          {protocolStatus && (
+            <p className="text-black text-sm font-medium mb-4 ml-4">
+              {JSON.stringify(protocolStatus)}
+            </p>
+          )}
 
           <div
-              onClick={handleCopy}
-              className="flex flex-row items-center justify-between text-slate-900 text-xs font-medium cursor-pointer"
+            onClick={handleQueryProtocol}
+            className="w-full py-4 mb-4 inline-flex items-center bg-gray-500  bg-opacity-10 opacity-70 justify-center text-center  cursor-pointer"
           >
-            Copy DID instead
-            <span>
-          <Image
-              className="absolute right-[3%] top-8"
-              src="/svg/cut.svg"
-              alt="Close Menu"
-              width={20}
-              height={20}
-          />
-
-          </span>
+            <h3 className="text-black text-sm font-medium">Protocol Query</h3>
           </div>
+          {queryProtocolStatus && (
+            <p className="text-black text-sm font-medium mb-4 ml-4">
+              {queryProtocolStatus}
+            </p>
+          )}
+
+          <div
+            onClick={handleSeeDid}
+            className="w-full py-4 inline-flex items-center bg-gray-500  bg-opacity-10 gap-2.5 opacity-70 justify-center text-center  cursor-pointer"
+          >
+            <h3 id="seeDidInfo" className="text-black text-sm font-medium">
+              See DID
+            </h3>
+          </div>
+          {isDidVisible && (
+            <div className="mt-4 p-4 bg-gray-200 rounded">
+              <p className="text-black text-sm font-medium">Your DID:</p>
+              <div className="overflow-auto max-h-40">
+                <pre className="text-xs break-words">{myDid}</pre>
+              </div>
+            </div>
+          )}
         </div>
-      </>
+        <Button
+          label="Proceed"
+          link={redirectUrl!}
+          className="w-full bg-accent py-4 rounded-md text-white text-center"
+        />
+
+        <div
+          onClick={handleCopy}
+          className="flex flex-row items-center justify-between text-slate-900 text-xs font-medium cursor-pointer"
+        >
+          Copy DID instead
+          <span>
+            <Image
+              src="/svg/tabler_copy.svg"
+              alt=""
+              width={15.47}
+              height={15.47}
+            />
+          </span>
+        </div>
+      </div>
+    </>
   );
 };
 
